2f651ae8605358a5e9968f3aecccbc6e8a128d70
Idwer Veltman <ir.veltman@gmail.com>
2021-12-31 13:43:18 +0100

    Added test and improved test

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,18 +6,34 @@ import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
 
-    @Test
-    fun foo() {
-        val items = arrayOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
+    lateinit var app: GildedRose
 
-        val item = app.items[0]
+    fun updateQuality(item: Item): Item {
+        app = GildedRose(arrayOf(item))
+        app.updateQuality()
+        return app.items[0]
+    }
+
+    @Test
+    fun qualityIsNeverNegative() {
+        val item = updateQuality(Item("foo", 0, 0))
 
         assertThat(item.name, equalTo("foo"))
+        // SellIn can be negative
         assertThat(item.sellIn, equalTo(-1))
-        assertThat(item.quality, equalTo(-1))
+        // Quality can't be negative
+        assertThat(item.quality, equalTo(0))
     }
+
+    @Test
+    fun qualityDegradesByTwoWhenSellByDateHasPassed() {
+        val item = updateQuality(Item("foo", -1, 40))
+
+        assertThat(item.sellIn, equalTo(-2))
+        // Quality can't be negative
+        assertThat(item.quality, equalTo(38))
+    }
+
 }
 
 
