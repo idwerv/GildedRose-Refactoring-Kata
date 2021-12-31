6017933c6f8b6508d293d3c3ebe8ad0636b70374
Idwer Veltman <ir.veltman@gmail.com>
2021-12-31 14:54:27 +0100

    More tests; Changed for-loop to for-each for readability; Added comments

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,56 +3,62 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+        for (item in items) {
+
+            // Decrease the -QUALITY- by 1, except for Aged Brie, Backstage passes and Sulfuras
+            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.quality > 0) {
+                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                        item.quality = item.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                // Aged Brie & Backstage passes -QUALITY- changes
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                    // Backstage passes only code
+                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            // Decrease -SELLIN- date by 1, except for Sulfuras
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.sellIn = item.sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != "Aged Brie") {
+                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.quality > 0) {
+                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                                item.quality = item.quality - 1
                             }
                         }
+                    // Backstage passes QUALITY = 0 when SellIn date is 0 or less.
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - item.quality
                     }
+                // Aged Brie
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
                     }
                 }
             }
         }
     }
-
 }
 
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -17,7 +17,6 @@ internal class GildedRoseTest {
     @Test
     fun qualityIsNeverNegative() {
         val item = updateQuality(Item("foo", 0, 0))
-
         assertThat(item.name, equalTo("foo"))
         // SellIn can be negative
         assertThat(item.sellIn, equalTo(-1))
@@ -28,11 +27,100 @@ internal class GildedRoseTest {
     @Test
     fun qualityDegradesByTwoWhenSellByDateHasPassed() {
         val item = updateQuality(Item("foo", -1, 40))
-
         assertThat(item.sellIn, equalTo(-2))
-        // Quality can't be negative
         assertThat(item.quality, equalTo(38))
     }
+
+    @Test
+    fun qualityDegradesByTwoWhenSellByDateHasPassedNeverNegative() {
+        val item = updateQuality(Item("foo", -1, 1))
+        assertThat(item.sellIn, equalTo(-2))
+        assertThat(item.quality, equalTo(0))
+    }
+
+    @Test
+    fun agedBrieQualityIncreasesBy1() {
+        val item = updateQuality(Item("Aged Brie", 10,0))
+        assertThat(item.sellIn, equalTo(9))
+        assertThat(item.quality, equalTo(1))
+    }
+
+    @Test
+    fun agedBrieQualityIncreasesBy2WhenSellByDateHasPassed() {
+        val item = updateQuality(Item("Aged Brie", -1,0))
+        assertThat(item.sellIn, equalTo(-2))
+        assertThat(item.quality, equalTo(2))
+    }
+
+    @Test
+    fun agedBrieQualityDoesntIncreaseOver50() {
+        val item = updateQuality(Item("Aged Brie", 0,50))
+        assertThat(item.sellIn, equalTo(-1))
+        assertThat(item.quality, equalTo(50))
+    }
+
+    @Test
+    fun agedBrieQualityDoesntIncreaseOver50EvenWhenSellByDatePassed() {
+        val item = updateQuality(Item("Aged Brie", -1,49))
+        assertThat(item.sellIn, equalTo(-2))
+        assertThat(item.quality, equalTo(50))
+    }
+
+    // Backstage passes = bp
+    @Test
+    fun bpQualityIncreasesBy1() {
+        val item = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 20,0))
+        assertThat(item.sellIn, equalTo(19))
+        assertThat(item.quality, equalTo(1))
+    }
+
+    @Test
+    fun bpQualityIncreasesBy2() {
+        var item = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 10,0))
+        assertThat(item.sellIn, equalTo(9))
+        assertThat(item.quality, equalTo(2))
+
+        item = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 6,0))
+        assertThat(item.sellIn, equalTo(5))
+        assertThat(item.quality, equalTo(2))
+    }
+
+    @Test
+    fun bpQualityIncreasesBy3() {
+        var item = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 5,0))
+        assertThat(item.sellIn, equalTo(4))
+        assertThat(item.quality, equalTo(3))
+
+        item = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 1,0))
+        assertThat(item.sellIn, equalTo(0))
+        assertThat(item.quality, equalTo(3))
+    }
+
+    @Test
+    fun bpQualityIsSetTo0WhenSellByDatePassed() {
+        val item = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 0,40))
+        assertThat(item.sellIn, equalTo(-1))
+        assertThat(item.quality, equalTo(0))
+    }
+
+    @Test
+    fun bpQualityNeverMoreThan50() {
+        val item = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 3,49))
+        assertThat(item.sellIn, equalTo(2))
+        assertThat(item.quality, equalTo(50))
+    }
+
+    @Test
+    fun sulfurasQualityAndSellInNeverChange() {
+        var item = updateQuality(Item("Sulfuras, Hand of Ragnaros", 50,50))
+        assertThat(item.sellIn, equalTo(50))
+        assertThat(item.quality, equalTo(50))
+
+        item = updateQuality(Item("Sulfuras, Hand of Ragnaros", 10,10))
+        assertThat(item.sellIn, equalTo(10))
+        assertThat(item.quality, equalTo(10))
+    }
+
 
 }
 
