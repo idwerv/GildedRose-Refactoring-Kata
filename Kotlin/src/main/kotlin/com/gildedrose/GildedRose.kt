496e1645d4b78de4da3ea762fa6403a48fab0e7a
Idwer Veltman <ir.veltman@gmail.com>
2022-01-02 21:22:05 +0100

    Refactored code again

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,45 +3,45 @@ package com.gildedrose
 private const val AGED_BRIE = "Aged Brie"
 private const val BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
 private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+private const val DEFAULT = ""
+
 
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            updateSellIn(item)
-            updateItemQuality(item)
+            val rule: QualityRule =
+                qualityRules.firstOrNull { it.name == item.name } ?: qualityRules.first { it.name == DEFAULT }
+            if (rule.sellInAdjustment) item.sellIn -= 1
+
+            item.quality =
+                (item.quality + rule.qualityIncrement(item) + pastDueDateAdjustment(item, rule)).coerceIn(0..50)
         }
     }
 
-    fun updateSellIn(item: Item) {
-        if (item.name != SULFURAS) item.sellIn -= 1
-    }
+    private fun pastDueDateAdjustment(item: Item, rule: QualityRule): Int =
+        if (item.sellIn >= 0) 0 else rule.pastDueDateAdjustment(item)
 
-    private fun updateItemQuality(item: Item) {
-        item.quality = when (item.name) {
-            AGED_BRIE -> item.quality + 1
-            BACKSTAGE_PASS -> {
-                when (item.sellIn) {
-                    in 0..4 -> item.quality + 3
-                    in 5..9 -> item.quality + 2
-                    in 10..49 -> item.quality + 1
-                    else -> 0
-                }
-            }
-            SULFURAS -> item.quality
-            else -> item.quality - 1
-        }.plus(getSellInQualityAdjustment(item))
-            .coerceIn(0..50)
-    }
-
-    private fun getSellInQualityAdjustment(item: Item): Int {
-        if (item.sellIn >= 0) return 0
-        return when (item.name) {
-            AGED_BRIE -> 1
-            BACKSTAGE_PASS, SULFURAS -> 0
-            else -> -1
-        }
-    }
 }
 
+class QualityRule(
+    val name: String,
+    val qualityIncrement: (Item) -> Int = { -1 },
+    val pastDueDateAdjustment: (Item) -> Int = { -1 },
+    val sellInAdjustment: Boolean = true,
+    val conjuredItem: Boolean = false
+)
 
+val qualityRules = arrayOf(
+    QualityRule(AGED_BRIE, { 1 }, { 1 }),
+    QualityRule(BACKSTAGE_PASS, { backstagePassRule(it) }, { -it.quality } ),
+    QualityRule(SULFURAS, { 0 }, { 0 }, false),
+    QualityRule(DEFAULT)
+)
+
+private fun backstagePassRule(it: Item) = when (it.sellIn) {
+    in 0..4 -> 3
+    in 5..9 -> 2
+    in 10..49 -> 1
+    else -> 0
+}
